48c6aef1c785fd4e4f68512da71958e20dc2417a
paruka <paruka.me@gmail.com>
2018-12-02 21:59:02 +0800

    add: 1. two sum

---
+++ cc/twoSum.cc
@@ -0,0 +1,28 @@
+/**********************************************************************************
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+Example:
+
+  Given nums = [2, 7, 11, 15], target = 9,
+
+  Because nums[0] + nums[1] = 2 + 7 = 9,
+  return [0, 1].
+**********************************************************************************/
+class Solution {
+public:
+  vector<int> twoSum(vector<int>& nums, int target) {
+    unordered_map<int, int> answers;
+    for (int i = 0; i < nums.size(); ++i) {
+      auto number = nums[i];
+      auto iter = answers.find(number);
+      if (iter != answers.end()) {
+        return {iter->second, i};
+      } else {
+        answers[target - number] = i;
+      }
+    }
+    return {};
+  }
+};
